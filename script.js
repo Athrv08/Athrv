5bdc67ca142372bef269ea299e511640196c1a61
Athrv08 <globalxpose@gmail.com>
2025-03-17 16:04:01 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+// Show alert on button click
+document.getElementById("showMessage").addEventListener("click", function() {
+    alert("Here are my projects! (Feature coming soon)");
+});
+
+// Dark mode toggle
+document.getElementById("toggle-dark-mode").addEventListener("click", function() {
+    document.body.classList.toggle("dark-mode");
+});
